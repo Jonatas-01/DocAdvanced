6a83b23699398d0f4eb5be165881d304d9bf57af
JonatasM <jonatasmc02@gmail.com>
2025-05-12 11:28:07 +0100

    refactor: hide SECRET_KEY

--- docadvanced/settings.py
+++ docadvanced/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rw$r#a76i59mmo$9c6-^3h(vnm&ih#knpaq&fqxeby3m1)1*ie'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
